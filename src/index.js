ec892d95161721f8bdbeca07597303434dcbde0d
Yevheniia Malakhova <jen.mala.kh@gmail.com>
2022-01-12 19:11:01 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import './sass/main.scss';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import ApiService from './js/api-service';
 import LoadMoreBtn from './js/load-more-btn';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from 'axios';
+import SimpleLightbox from "simplelightbox";
+
+const apiService = new ApiService();
+const loadMoreBtn = new LoadMoreBtn('.load-more-btn');
+const lightbox = new SimpleLightbox('.gallery a');
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
@@ -12,41 +17,47 @@ const refs = {
 
 refs.searchForm.addEventListener('submit', searchImagesByWord);
 refs.loadMoreBtn.addEventListener('click', loadMoreImages);
-    
 
-
-const apiService = new ApiService();
-const loadMoreBtn = new LoadMoreBtn('.load-more-btn');
-
-
-
-function searchImagesByWord(e) {
+async function searchImagesByWord(e) {
     e.preventDefault();
-
     loadMoreBtn.show();
-    loadMoreBtn.disable();
-        
+    loadMoreBtn.disable();        
     refs.imagesContainer.innerHTML = '';
     apiService.resetPage();
     apiService.query = e.currentTarget.elements.searchQuery.value;
+
     if (apiService.query === '') {
         loadMoreBtn.hide();
-        return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        Notify.failure('Еnter text!');
+        return; 
     }
-    apiService.fetchImages()
-        .then(renderMarkup)
-        .catch(() => { loadMoreBtn.hide() });
+    try {
+        const images = await apiService.fetchImages();
+        renderMarkup(images);
+    } catch {
+        loadMoreBtn.hide();
+    }   
+    loadMoreBtn.enable();
+
 }
 
-function loadMoreImages() {
+async function loadMoreImages() {
     loadMoreBtn.disable();
-    apiService.fetchImages().then(renderMarkup);
-    // if ((imgPerPage*this.page) >= data.total) {
-    //     Notify.info(`We're sorry, but you've reached the end of search results.`);
-    //     loadMoreBtn.hide();
-    //     return;
-    // }
-    loadMoreBtn.enable();
+    try {
+        const images = await apiService.fetchImages();
+        renderMarkup(images);
+
+        // плавная прокрутка
+        const { height: cardHeight } = document.querySelector('.gallery').firstElementChild.getBoundingClientRect();
+
+        window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+        });
+    } catch {
+        loadMoreBtn.hide();
+    }
+    loadMoreBtn.enable();    
 }
 
 
@@ -54,35 +65,33 @@ function renderMarkup(elements) {
     const markup = elements.map(element => {
         return `
         <div class="photo-card">
-            <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" />
-                <div class="info">
-                    <p class="info-item">
-                        <b>Likes</b>
-                        ${element.likes}
-                    </p>
+            <a href ="${element.largeImageURL}">
+                <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" />
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes</b>
+                            ${element.likes}
+                        </p>
 
-                    <p class="info-item">
-                        <b>Views</b>
-                        ${element.views}
-                    </p>
+                        <p class="info-item">
+                            <b>Views</b>
+                            ${element.views}
+                        </p>
 
-                    <p class="info-item">
-                        <b>Comments</b>
-                        ${element.comments}
-                    </p>
+                        <p class="info-item">
+                            <b>Comments</b>
+                            ${element.comments}
+                        </p>
 
-                    <p class="info-item">
-                        <b>Downloads</b>
-                        ${element.downloads}
-                    </p>
-                </div>
+                        <p class="info-item">
+                            <b>Downloads</b>
+                            ${element.downloads}
+                        </p>
+                    </div>
+            </a>
         </div>`;
     }).join('');
 
     refs.imagesContainer.insertAdjacentHTML('beforeend', markup);
-
-    loadMoreBtn.enable();
+    lightbox.refresh();   
 }
-
-        
-
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,5 +1,7 @@
 import LoadMoreBtn from './load-more-btn';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const loadMoreBtn = new LoadMoreBtn('.load-more-btn');
 
@@ -13,29 +15,27 @@ export default class ApiService {
         this.page = 1;
     }    
 
-    fetchImages() {
+    async fetchImages() {
         const url = `${BASE_URL}?image_type=photo&orientation=horizontal&safesearch=true&q=${this.searchQuery}&page=${this.page}&per_page=${imgPerPage}&key=${API_KEY}`
-        return fetch(url)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error(response.statusText);
-            })
-            .then(data => {
-                if (data.hits.length === 0) {
-                    Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-                    throw new Error(data.statusText);
-                }
+        const response = await axios.get(url);
 
-                if ((imgPerPage*this.page) >= data.totalHits) {
-                    Notify.info(`We're sorry, but you've reached the end of search results.`);    
-                    loadMoreBtn.hide();
-                }
-                console.log(data);
-                this.page += 1;
-                return data.hits;
-            });
+        if (response.data.hits.length === 0) {
+            Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            throw new Error(data.statusText);
+        }
+
+        if (this.page === 1) {
+            Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+        }
+
+        if ((imgPerPage*this.page) >= response.data.totalHits) {             
+            loadMoreBtn.hide();
+            Notify.info(`We're sorry, but you've reached the end of search results.`);                
+        } else {
+            this.page += 1;
+        }
+
+        return response.data.hits;            
     }
 
     resetPage() {
